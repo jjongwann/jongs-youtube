0d710196e03be18a167254e73d232e8fe4a30cfe
Jongs <143797909+jjongwann@users.noreply.github.com>
2023-11-05 11:25:40 +0900

    메인 콘텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
       <footer id='footer' role='contentinfo'>
-        Footer
+        <Link to={'mailto:whddhks1121.naver.com'} rel='noopenner noreferrer'>
+            whddhks1121.naver.com
+        </Link>
       </footer>
     )
 }
