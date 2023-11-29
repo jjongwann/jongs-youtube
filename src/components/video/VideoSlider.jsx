3d34e3f1ad04d25373cfd5f283fe2d5901d0257c
Jongs <143797909+jjongwann@users.noreply.github.com>
2023-11-29 15:07:56 +0900

    Swiper 이미지 슬라이드 작업

--- src/components/video/VideoSlider.jsx
+++ src/components/video/VideoSlider.jsx
@@ -1,56 +1,59 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
+
 import { Navigation } from 'swiper/modules';
 
-const VideoSlider = ({id, title, videos}) => {
-    const [loading, setLoading] = useState(true);
+const VideoSlider = ({ videos, title, id }) => {
+    const [loading, setLoading] = useState(true); 
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            setLoading(false)
-        },500)
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 500);
     }, []);
 
-    const videoClass = loading ? 'isLoading' : 'isLoaded';
+    const youtubeClass = loading ? 'isLoading' : 'isLoaded';
 
     return (
-        <section id={id} className={videoClass}>
+        <section id={id} className={youtubeClass}>
             <h2>{title}</h2>
+
             <div className='video__slider'>
-                <Swiper 
-                    slidesPerView={1} 
-                    spaceBetween={20} 
-                    navigation={true} 
-                    modules={[Navigation]} 
-                    className={`mySwiper-${id}`} 
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={20}
+                    navigation={true}
+                    modules={[Navigation]}
+                    className={`mySwiper-${id}`}
                     breakpoints={{
                         640: {
                             slidesPerView: 2,
-                            spaceBetween: 20
+                            spaceBetween: 20,
                         },
                         768: {
                             slidesPerView: 3,
-                            spaceBetween: 20
+                            spaceBetween: 20,
                         },
                         1024: {
                             slidesPerView: 4,
-                            spaceBetween: 20
+                            spaceBetween: 20,
                         },
                         1600: {
-                            slidesPerView: 5,
-                            spaceBetween: 20
-                        }
+                            slidesPerView: 4,
+                            spaceBetween: 20,
+                        },
                     }}
                 >
-                    {videos.map((video, key)=>(
+                    {videos.map((video, key) => (
                         <SwiperSlide key={key}>
-                            <div className='video'>
-                                <div className='video__thumb play__icon'>
+                            <div className="video" key={key}>
+                                <div className="video__thumb play__icon">
                                     <Link to={`/video/${video.videoId}`}>
-                                        <img src={video.img} alt={video.title}/>
+                                        <img src={video.img} alt={video.title} />
                                     </Link>
                                 </div>
                             </div>
