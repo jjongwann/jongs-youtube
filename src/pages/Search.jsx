63092a9ae54ab4fd5a68239ecdde36bc200041a9
Jongs <143797909+jjongwann@users.noreply.github.com>
2023-12-01 15:17:16 +0900

    Rapid API 및 더보기 설정하기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,27 +2,50 @@ import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
 import VideoSearch from '../components/video/VideoSearch'
 import { useParams } from 'react-router-dom'
+import { fetchFromAPI } from '../utils/api'
+
 const Search = () => {
     const { searchId } = useParams();
     const [videos, setVideos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [nextPageToken, setNextPageToken] = useState(null);
 
     useEffect(() => {
-        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${searchId}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
-        .then(Response => Response.json())
-        .then(result => {
-            console.log(result);
-            setVideos(result.items);
-        })
-        .catch(error => console.log(error));
+       setVideos([]);
+       fetchVideos(searchId);
+       setLoading(true);
     }, [searchId]);
+
+    const fetchVideos = (query, pageToken = '') => {
+        fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+       .then((data) => {
+        setNextPageToken(data.nextPageToken);
+        setVideos((preVideos) => [...preVideos, ...data.items]); //CONCAT과 같은 역할(이전 데이터 + 다음 데이터)
+        setLoading(false);
+    })
+    .catch((error)=>{
+        console.log('Error fetching data', error);
+    })
+    }
+
+    const handleLoadMore = () => {
+        if(nextPageToken){
+            fetchVideos(searchId, nextPageToken);
+        }
+    }
+
+    const searchPageClass = loading ? 'isLoading' : 'isLoaded';
 
     return (
         <Main 
             title = "유투브 검색"
             description="유튜브 검색 결과 페이지입니다.">
-            <section id='searchPage'>
+            <section id='searchPage' className={searchPageClass}>
                 <div className='video__inner search'>
                     <VideoSearch videos={videos}/>
+                </div>
+                <div className='video__more'>
+                    {nextPageToken && (<button onClick={handleLoadMore}>더 보기</button>)} 
                 </div>
             </section>
         </Main>
