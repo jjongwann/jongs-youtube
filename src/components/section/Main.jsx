928f8b56cf3e9ae47836efc7743075ee041aa271
Jongs <143797909+jjongwann@users.noreply.github.com>
2023-12-06 17:48:03 +0900

    배경 이미지 수정2

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -17,7 +17,7 @@ const Main = (props) => {
             >
                 {props.title && <title>{props.title}</title>}
                 <meta name="description" content={props.description} />
-                
+                <meta property="og:img" content="https://yt3.googleusercontent.com/PVbwh8OLmp3dTofJ7vmv6zfoRKiFgIvL9SsTYaermGxW3Ga6vONSfC7ymrgpO6gUCd9nKcZp0Sc=s176-c-k-c0x00ffffff-no-rj"/>
             </Helmet>
 
             <Header />
