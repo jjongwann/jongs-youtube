3dcd368da07f2d8405e9fa8d3931489c3c8a6bed
Jongs <143797909+jjongwann@users.noreply.github.com>
2023-12-06 17:44:05 +0900

    배경 이미지 수정

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -17,7 +17,7 @@ const Main = (props) => {
             >
                 {props.title && <title>{props.title}</title>}
                 <meta name="description" content={props.description} />
-                <meta property='og:img' content="https://jongs-youtube.netlify.app/../../aasets/img/jongsYoutube.png"/>
+                
             </Helmet>
 
             <Header />
